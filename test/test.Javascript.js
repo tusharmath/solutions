b90244c132d77c3212858b4f476872114a5df243
tusharmath <tusharmath@gmail.com>
2015-06-03 15:07:58 +0530

    add test case

--- test/test.Javascript.js
+++ test/test.Javascript.js
@@ -52,11 +52,13 @@ describe('Tag', function () {
             tag2.addAttribute('a2', 'v2');
             tag2.addAttribute('a22', 'v22');
             tag3.addAttribute('a3', 'v3');
+            tag3.addAttribute('a22', 'v22');
 
             rootTag.findByAttributes([{name: 'a1', value: 'v1'}]).should.deep.equal([tag1]);
             rootTag.findByAttributes([{name: 'a2', value: 'v2'}, {name: 'a22', value: 'v22'}]).should.deep.equal([tag2]);
             rootTag.findByAttributes([{name: 'a2', value: 'v2'}, {name: 'a23', value: 'v23'}]).should.be.empty;
             rootTag.findByAttributes([{name: 'a3', value: 'v3'}]).should.deep.equal([tag3]);
+            rootTag.findByAttributes([{name: 'a22', value: 'v22'}]).should.deep.equal([tag2, tag3]);
         });
     });
 
