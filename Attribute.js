76669d0da171e910a0621da9b5fe32ba7ebc8335
tusharmath <tusharmath@gmail.com>
2015-06-04 09:25:36 +0530

    add ng-docs

--- Attribute.js
+++ Attribute.js
@@ -2,13 +2,28 @@
  * Created by tusharmathur on 6/3/15.
  */
 "use strict";
+/**
+ * Attribute class
+ * @class
+ * @type {Attribute}
+ */
 module.exports = class Attribute {
+    /**
+     * Constructor
+     * @param {string} name
+     * @param {object} value
+     */
     constructor(name, value) {
         this.name = name;
         this.value = value;
         this.equals = this.equals.bind(this);
     }
 
+    /**
+     * Check for equality of {attribute}
+     * @param {Attribute} attribute
+     * @returns {boolean}
+     */
     equals(attribute) {
         return attribute.name === this.name && attribute.value === this.value;
     }
--- Tag.js
+++ Tag.js
@@ -28,16 +28,57 @@ var tagHasAllAttributes = function (searchAttributes, tag) {
 var matchKeyValueForObject = function (key, value, obj) {
     return obj[key] === value;
 };
+/**
+ * Node of a tag tree
+ * @class
+ */
 class Tag {
     constructor() {
+
+        /**
+         * Link to parent tag
+         * @type {Tag}
+         */
         this.parent = null;
+
+        /**
+         * Collection of children tags
+         * @type {Tag[]}
+         */
         this.children = [];
+
+        /**
+         * Attribute Collection
+         * @type {Attribute[]}
+         */
         this.attributes = [];
+
+        /**
+         * Search tags recursively by attributes, through the child nodes
+         * @param {Attribute[]} search attributes
+         * @returns {Tag[]}
+         */
         this.findByAttributes = _.partial(this._createSearchStrategy, invokeOnChildren);
+
+        /**
+         * Search tags linearly by attributes, through its parent nodes
+         * @param {Attribute[]} search attributes
+         * @returns {Tag[]}
+         */
         this.findParent = _.partial(this._createSearchStrategy, invokeOnParent);
+
+        /**
+         * Search tags recursively through the child nodes, return and caches the response
+         * @param {Attribute[]} search attributes
+         * @returns {Tag[]}
+         */
         this.findByAttributesMemoized = _.memoize(this.findByAttributes, _.identity);
     }
 
+    /**
+     * Add a tag to another node as child
+     * @param {Tag} tag
+     */
     addChild(tag) {
         if (tag.parent) {
             throw Error("Can not re add a node");
@@ -46,14 +87,29 @@ class Tag {
         this.children.push(tag);
     }
 
+    /**
+     * Create a search strategy
+     * @param {Function} invokeOn
+     * @param {Attribute[]} attributes
+     * @private
+     */
     _createSearchStrategy(invokeOn, attributes) {
         return recursiveFilter(invokeOn, _.partial(tagHasAllAttributes, attributes), [], this);
     }
 
+    /**
+     * Add an attribute by name to the tag
+     * @param {String} name
+     * @param {Object} value
+     */
     addAttribute(name, value) {
         this.attributes.push(new Attribute(name, value));
     }
 
+    /**
+     * Remove an attribute by name from the tag
+     * @param {String} name
+     */
     removeAttribute(name) {
         _.remove(this.attributes, _.partial(matchKeyValueForObject, 'name', name));
     }
