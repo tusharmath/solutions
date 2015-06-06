4476e695bff0c2e2fa9880c425a4795035f54d19
tusharmath <tusharmath@gmail.com>
2015-06-06 14:57:15 +0530

    add ng-docs

--- Tag.js
+++ Tag.js
@@ -2,53 +2,112 @@
 var _ = require('lodash');
 var TagAttribute = require('./TagAttribute');
 
+/**
+ * Traverses the tree using a generator to get to the next node
+ * @param {function} generator
+ * @param {function} arrayMapper
+ * @param {Tag} node
+ * @param {object} params
+ * @returns {Array}
+ */
+var treeToArray = function (generator, arrayMapper, node, params) {
+    var results = [];
+    results.push(params);
+    eachOf(generator(node), function (child) {
+        results = results.concat(treeToArray(generator, arrayMapper, child, arrayMapper(child, params, node)));
+    });
+    return results;
+};
+/**
+ * Generator to get the parent node
+ * @param {Tag} node
+ */
 var getParentAsIterable = function * (node) {
     if (node) {
         yield node.parent;
     }
 };
 
+/**
+ * Generator to get the child nodes
+ * @param {Tag} node
+ * @yield {Tag}
+ */
 var getChildrenAsIterable = function * (node) {
     if (node) {
         yield * node.children;
     }
 };
 
+/**
+ * Calls an action over each iteration
+ * @param {Symbol.iterator} iterator
+ * @param {function} action
+ */
 var eachOf = function (iterator, action) {
     for (var val of iterator) {
         action(val);
     }
 };
 
-var createNodeName = function (params) {
+/**
+ * Creates string to print the node
+ * @param {Object} params
+ * @returns {string}
+ */
+var createNodePrintString = function (params) {
     return params.prefix + (params.isTail ? "└── " : "├── ") + arraySerialize(params.node.attributes);
 };
 
-var printIterator = function (child, params, parent) {
+/**
+ * Mapper function for iterating over the tree
+ * @param {Tag} child
+ * @param {object} params
+ * @param {Tag} parent
+ * @returns {{prefix: string, isTail: boolean, node: Tag}}
+ */
+var printMapper = function (child, params, parent) {
     return {
         prefix: params.prefix + (params.isTail ? "    " : "│   "),
         isTail: child === _.last(parent.children),
         node: child
     };
 };
-var recursiveIterator = function (generator, iterator, node, params) {
-    var results = [];
-    results.push(params);
-    eachOf(generator(node), function (child) {
-        results = results.concat(recursiveIterator(generator, iterator, child, iterator(child, params, node)));
-    });
-    return results;
-};
-
+/**
+ * Checks if {searchAttribute} is present on a tag
+ * @param {Attribute} searchAttribute
+ * @param {Tag} tag
+ * @returns {boolean}
+ */
 var tagHasAttribute = function (searchAttribute, tag) {
     return tag && _.any(tag.attributes, searchAttribute.equals);
 };
+/**
+ * Checks is all the attributes are present on a tag
+ * @param {Attribute} searchAttributes
+ * @param {Tag} tag
+ * @returns {boolean}
+ */
 var tagHasAllAttributes = function (searchAttributes, tag) {
     return _.all(searchAttributes, _.partial(_.rearg(tagHasAttribute, 1, 0), tag));
 };
-var matchKeyValueForObject = function (key, value, obj) {
+
+/**
+ * Checks if {value} matches obj[key]
+ * @param {string} key
+ * @param {object} value
+ * @param {object} obj
+ * @returns {boolean}
+ */
+var hasValue = function (key, value, obj) {
     return obj[key] === value;
 };
+
+/**
+ * Serializes an array of attributes
+ * @param {Attribute[]} list
+ * @returns {string}
+ */
 var arraySerialize = function (list) {
     return _.invoke(list, 'toString').join(', ');
 };
@@ -121,7 +180,7 @@ class Tag {
      */
     _createSearchStrategy(generator, attributes) {
         var attributeSieve = _.partial(tagHasAllAttributes, attributes);
-        return _.filter(recursiveIterator(generator, _.identity, this, this), attributeSieve);
+        return _.filter(treeToArray(generator, _.identity, this, this), attributeSieve);
     }
 
     /**
@@ -138,18 +197,18 @@ class Tag {
      * @param {String} name
      */
     removeAttribute(name) {
-        _.remove(this.attributes, _.partial(matchKeyValueForObject, 'name', name));
+        _.remove(this.attributes, _.partial(hasValue, 'name', name));
     }
 
     /**
      * Converts the tree from the current node to an iterable array (DFS:Pre order)
      */
     toString() {
-        return _.map(recursiveIterator(getChildrenAsIterable, printIterator, this, {
+        return _.map(treeToArray(getChildrenAsIterable, printMapper, this, {
             isTail: true,
             prefix: '',
             node: this
-        }), createNodeName).join('\n');
+        }), createNodePrintString).join('\n');
     }
 }
 module.exports = Tag;
