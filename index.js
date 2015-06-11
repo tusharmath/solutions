f5f3f990c948b9e28f18753f8c19d64294929b88
tusharmath <tusharmath@gmail.com>
2015-06-11 22:45:52 +0530

    --wip--

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
-/**
- * Created by tusharmathur on 6/6/15.
- */
-module.exports = require('./Tag');
+"use strict";
+var index = 0;
+var input = [
+    2, 3, 10, 2, 4, 8, 1
+    //3, 2, 13, 11, 7, 9, 5, 6, 1, 2, 17
+];
+
+var min = input[0], maxDiff = input[1] - input[0];
+
+for (var i = 1; i < input.length; i++) {
+    var ith = input[i];
+
+    var diff = ith - min;
+    if (diff > maxDiff) {
+        maxDiff = diff;
+    }
+    if (ith < min) {
+        min = ith;
+    }
+
+    console.log(min, ith, maxDiff);
+}
+
+return maxDiff;
+
