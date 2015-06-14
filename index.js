d2c2b02ce5cf52d1e5268b4a913f32ad51acfb19
tusharmath <tusharmath@gmail.com>
2015-06-14 13:43:02 +0530

    --wip--

--- index.js
+++ index.js
@@ -1,4 +1,38 @@
-/**
- * Created by tusharmathur on 6/6/15.
- */
-module.exports = require('./Tag');
+(function () {
+    "use strict";
+
+    function Grid(size, count, element) {
+        var blockSize = size,
+            viewPort = size * count,
+            c_canvas = element,
+            context = c_canvas.getContext("2d");
+        c_canvas.height = c_canvas.width = viewPort + blockSize * 2;
+        this.render = function () {
+            _.times(viewPort / blockSize, function (i) {
+                var pos = i * blockSize + 0.5 + blockSize;
+                context.moveTo(blockSize, pos);
+                context.lineTo(viewPort, pos);
+                context.moveTo(pos, blockSize);
+                context.lineTo(pos, viewPort);
+            });
+            context.strokeStyle = "#000";
+            context.stroke();
+        };
+    }
+
+    function Snake(direction) {
+        var movers = [
+            function up () {
+
+            }
+        ];
+        this.move = function () {
+            movers[direction]();
+        };
+
+        this.turn = function (_direction) {
+            direction = _direction;
+        }
+    }
+
+})();
