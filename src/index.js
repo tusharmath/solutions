b5b3a351036ee426dc77503d169cc591cd203bfb
tusharmath <tusharmath@gmail.com>
2015-06-28 17:00:00 +0530

    refactor

--- src/index.js
+++ src/index.js
@@ -1,20 +1,58 @@
-var randomDirection = function () {
-    return ['L', 'R', 'U', 'D'][_.random(0, 3)];
-};
 (function () {
     "use strict";
-
     var CELL_WIDTH = 20,
         LINE_WIDTH = 1,
         BLOCK_COLOR = '#F00',
         CELL_COUNT = 20,
         toXY = function (pos) {
-            return {
-                x: (pos[0] + 1 ) * CELL_WIDTH,
-                y: (pos[1] + 1) * CELL_WIDTH
-            };
+            return {x: pos[0], y: pos[1]};
         },
-        lastXY = _.flowRight(toXY, _.last);
+        toCell = function (pos) {
+            pos.x = (pos.x + 1) * CELL_WIDTH;
+            pos.y = (pos.y + 1) * CELL_WIDTH;
+            return pos;
+        },
+        randomDirection = function () {
+            return ['L', 'R', 'U', 'D'][_.random(0, 3)];
+        },
+        normalizeStep = function (lastStep, currentStep) {
+            var skipSteps = {
+                L: 'R',
+                R: 'L',
+                U: 'D',
+                D: 'U'
+            };
+            if (currentStep === skipSteps[lastStep]) {
+                return lastStep;
+            }
+            return currentStep;
+        },
+        getStep = function (key) {
+            return toXY({
+                L: [-1, 0],
+                R: [1, 0],
+                U: [0, -1],
+                D: [0, 1]
+            }[key]);
+        },
+        normalizeNode = function (CELL_COUNT, node) {
+            var MAX_X = CELL_COUNT - 1,
+                MAX_Y = CELL_COUNT - 2;
+            if (node[1] >= MAX_X) {
+                node[1] = 0;
+            }
+            if (node[1] < 0) {
+                node[1] = MAX_Y;
+            }
+
+            if (node[0] >= MAX_X) {
+                node[0] = 0;
+            }
+
+            if (node[0] < 0) {
+                node[0] = MAX_Y;
+            }
+        };
 
     var Views = (function (element) {
         var context = element.getContext("2d"),
@@ -24,7 +62,7 @@ var randomDirection = function () {
             };
 
         function GridView(grid) {
-            var CELL_width = grid.cellWidth
+            var CELL_width = grid.cellWidth;
             var viewPort = CELL_WIDTH * CELL_COUNT;
             element.height = element.width = viewPort + CELL_WIDTH * 2;
             this.render = function () {
@@ -44,8 +82,8 @@ var randomDirection = function () {
         function SnakeView(snake) {
             var _drawBlock = _.curry(drawBlock);
             this.render = function () {
-                _.each(_.map(snake.print, toXY), _drawBlock(BLOCK_COLOR));
-                _drawBlock('#FFF')(toXY(snake.clear));
+                _.each(_(snake.printSteps).map(toXY).map(toCell).value(), _drawBlock(BLOCK_COLOR));
+                _drawBlock('#FFF')(toCell(toXY(snake.clearSteps)));
             };
         }
 
@@ -59,49 +97,18 @@ var randomDirection = function () {
         }
 
         function Snake(list) {
-            var movements = {
-                L: [-1, 0],
-                R: [1, 0],
-                U: [0, -1],
-                D: [0, 1]
-            };
-            var ignoredMovements = {
-                L: 'R',
-                R: 'L',
-                U: 'D',
-                D: 'U'
-            };
-            this.print = list;
-            this.clear = {};
-            this.lastDirection = null;
-
-            this.move = function (key) {
-                if (key === ignoredMovements[this.lastDirection]) {
-                    key = this.lastDirection;
-                }
-                this.lastDirection = key;
+            this.printSteps = list;
+            this.clearSteps = {};
+            this.lastStep = null;
+            this.move = function (step) {
+                step = normalizeStep(this.lastStep, step);
                 var last = _.last(list);
-                this.clear = list.shift();
-                var node = [last[0] + movements[key][0], last[1] + movements[key][1]];
-                var MAX_X = CELL_COUNT - 1,
-                    MAX_Y = CELL_COUNT - 2;
-                if (node[1] >= MAX_X) {
-                    node[1] = 0;
-                }
-                if (node[1] < 0) {
-                    node[1] = MAX_Y;
-                }
-
-                if (node[0] >= MAX_X) {
-                    node[0] = 0;
-                }
-
-                if (node[0] < 0) {
-                    node[0] = MAX_Y;
-                }
-
-                console.log(_.find(list, node));
+                this.clearSteps = list.shift();
+                var _movements = getStep(step);
+                var node = [last[0] + _movements.x, last[1] + _movements.y];
+                normalizeNode(CELL_COUNT, node);
                 list.push(node);
+                this.lastStep = step;
             }.bind(this);
         }
 
@@ -126,7 +133,7 @@ var randomDirection = function () {
         var onRequest = function () {
             move(randomDirection());
         };
-        setInterval(onRequest, 1000);
+        setInterval(onRequest, 200);
     })(Views);
 
 })();
