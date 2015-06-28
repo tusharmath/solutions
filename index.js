32c807ad04b1d7021b0dbb1d1f9f82633a40fc40
tusharmath <tusharmath@gmail.com>
2015-06-28 12:56:08 +0530

    able to draw snake

--- index.js
+++ index.js
@@ -1,28 +1,50 @@
-(function () {
+var Views = (function (element) {
     "use strict";
+    var context = element.getContext("2d"),
+        cellSize = 20,
+        cellCount = 20,
+        toXY = function (pos) {
+            return {
+                x: (pos[0] + 1 ) * cellSize,
+                y: (pos[1] + 1) * cellSize
+            };
+        },
+        drawBlock = function (pos) {
+            context.fillStyle = "#F00";
+            context.fillRect(pos.x, pos.y, cellSize, cellSize);
+        };
 
-    function Grid(size, count, element) {
-        var blockSize = size,
-            viewPort = size * count,
-            c_canvas = element,
-            context = c_canvas.getContext("2d");
-        c_canvas.height = c_canvas.width = viewPort + blockSize * 2;
+    function GridView() {
+        var viewPort = cellSize * cellCount;
+        element.height = element.width = viewPort + cellSize * 2;
         this.render = function () {
-            _.times(viewPort / blockSize, function (i) {
-                var pos = i * blockSize + 0.5 + blockSize;
-                context.moveTo(blockSize, pos);
+            _.times(viewPort / cellSize, function (i) {
+                var pos = i * cellSize + 0.5 + cellSize;
+                context.moveTo(cellSize, pos);
                 context.lineTo(viewPort, pos);
-                context.moveTo(pos, blockSize);
+                context.moveTo(pos, cellSize);
                 context.lineTo(pos, viewPort);
             });
-            context.strokeStyle = "#000";
+            context.strokeStyle = "#cef";
             context.stroke();
         };
     }
 
+    function SnakeView(snake) {
+        this.render = function () {
+            var positions = _.map(snake, toXY);
+            _.each(positions, drawBlock);
+        };
+    }
+
+    return {GridView, SnakeView};
+})(document.getElementById('game'));
+
+(function (views, el) {
+    "use strict";
     function Snake(direction) {
         var movers = [
-            function up () {
+            function up() {
 
             }
         ];
@@ -35,4 +57,16 @@
         }
     }
 
-})();
+    var g = new views.GridView();
+    var s = new views.SnakeView([
+        [0, 1],
+        [1, 1],
+        [1, 2],
+        [1, 3],
+        [1, 4],
+        [2, 4]
+    ]);
+    g.render();
+    s.render();
+
+})(Views);
