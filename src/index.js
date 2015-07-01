0628bca8fce088e4e83e0b45ca9377c7ce394a4e
tusharmath <tusharmath@gmail.com>
2015-07-02 00:44:49 +0530

    able to extract content ffrom dom

--- src/index.js
+++ src/index.js
@@ -5,19 +5,23 @@
     "use strict";
 
     var $ = document.querySelector.bind(document),
+        $newTransaction = $('#new-transaction'),
+        TRANSACTION_FIELDS = ['amount', 'payees', 'payer', 'description'],
+        transactions = [],
         _map = function (list, cb, ctx) {
             var results = [];
             for (var i = 0; i < list.length; i++) {
                 var obj = list[i];
-                results.push(cb.call(ctx || null, obj));
+                results.push(cb.call(ctx || null, obj, i));
             }
             return results;
         },
         _inputData = function (el, keys) {
             return _map(keys, function (key) {
+                var elements = el.elements;
                 return {
                     key: key,
-                    value: el.elements[key] ? el.elements[key].value : null
+                    value: elements[key] ? elements[key].value : null
                 }
             });
         },
@@ -27,9 +31,57 @@
                 obj[i.key] = i.value;
             });
             return obj
-        };
+        },
+        _identity = function (x) {
+            return x;
+        },
+        _toArray = function (item) {
+            return _map(item, _identity);
+        },
+        _first = function (list) {
+            return list [0];
+        },
+        _head = function (arr) {
+            return _first(arr.slice(0, 1));
+        },
+        _body = function (arr) {
+            return arr.slice(1);
+        },
 
-    var data = _inputData($('#new-transaction'), ['amount', 'payees', 'payer', 'description']);
-    console.log(_toObject(data));
+        _flow = function () {
+            var funcList = _toArray(arguments).reverse(),
+                funcHead = _head(funcList),
+                funcBody = _body(funcList);
+
+            return function () {
+                var temp = funcHead.apply(null, arguments);
+                _map(funcBody, function (func) {
+                    temp = func(temp);
+                });
+                return temp;
+            };
+        },
+        _tokenize = function (str) {
+            if (!str || str.length <= 0) {
+                return [];
+            }
+            return _map(str.split(','), function (str) {
+                return str.trim();
+            });
+        },
+        _transaction = function (transactionRaw) {
+            return {
+                payees: _tokenize(transactionRaw.payees),
+                amount: parseInt(transactionRaw.amount, 10),
+                payer: transactionRaw.payer.trim()
+            }
+        },
+        getTransactionFromForm = _flow(_transaction, _toObject, _inputData);
+
+    $('#create').addEventListener('click', function () {
+
+        var transaction = getTransactionFromForm($newTransaction, TRANSACTION_FIELDS);
+        console.log(transaction);
+    });
 
 })();
