9e6c0c3f3b7da39d153a89ac3dc7eb22561cf53a
tusharmath <tusharmath@gmail.com>
2015-07-08 02:09:27 +0530

    rename

--- src/public/scripts/ContactDetailsController.js
+++ src/public/scripts/ContactDetailsController.js
@@ -1,15 +1,12 @@
 /**
  * Created by tusharmathur on 7/8/15.
  */
-/**
- * Created by tusharmathur on 7/8/15.
- */
 "use strict";
 var co = require('co'),
-    AsyncWatcher = require('./AsyncWatcher');
+    watch = require('./AsyncWatcher');
 
 var ContactSearchController = co.wrap(function * (rest, $scope) {
-    for (let i of AsyncWatcher($scope, 'selected')) {
+    for (let i of watch($scope, 'selected')) {
         let selectedId = (yield i).newValue;
         if (selectedId) {
             $scope.contact = yield rest.one('contacts', selectedId).get();
