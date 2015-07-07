a263dc72afdf32f8a422d51ca80defe45ec861fc
tusharmath <tusharmath@gmail.com>
2015-07-08 03:50:16 +0530

    support search via side bar

--- src/public/scripts/AddressBookController.js
+++ src/public/scripts/AddressBookController.js
@@ -7,13 +7,21 @@ var co = require('co'),
 
 var AddressBookController = co.wrap(function * (rest, $scope) {
     var contactsR = rest.all('contacts');
+    $scope.searchText = '';
     $scope.contacts = yield contactsR.getList();
+    $scope.charGroups = _.keys(_.groupBy($scope.contacts, function (contact) {
+        return contact.first_name[0].toLowerCase();
+    })).sort();
     $scope.create = false;
     $scope.edit = false;
 
     $scope.setSelected = function (id) {
         $scope.selected = id;
     };
+    $scope.setSearchText = function (str) {
+        $scope.searchText = str;
+    };
+
     $scope.setSelected(_.first($scope.contacts)._id);
     $scope.save = co.wrap(function * () {
         yield contactsR.post($scope.newContact);
