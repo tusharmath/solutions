4122f4b5fdc780261cbcbda57640286ce642ae09
Tushar Mathur <tusharmath@gmail.com>
2016-11-27 14:58:43 +0530

    --wip-- [skip ci]

--- src/view.js
+++ src/view.js
@@ -37,21 +37,35 @@ const UniqCarTypes = R.compose(R.uniq, R.pluck('type'))
 const CarType = R.map(type => h('option', type))
 
 const FilterHeader = (cars) => {
-
   return h('div.filter-header', [
     h('h3', ['Available Cars']),
     h('div.filters', [
-      h('select', CarType(UniqCarTypes(cars)))
+      h('select', CarType(R.prepend('any', UniqCarTypes(cars))))
     ])
   ])
 }
 
+function Confirmation (dispatcher) {
+  const props = {on: {click: dispatcher.of('select').listen.bind(null, undefined)}};
+  return h('div.confirmation', [
+    h('div.dialog', [
+      h('h4', ['Confirm booking?']),
+      h('p', ['I agree to terms and condition, is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.']),
+      h('div.footer', [
+        h('div.btn.btn-link.warning', props, ['AGREE']),
+        h('div.btn.btn-link.primary', props, ['DISAGREE'])
+      ]),
+    ])
+  ]);
+}
 module.exports = (dispatcher, model) => {
+
   return h('div', [
     h('div.nav-main', ['Zoom Car Rentals']),
     h('div.content', [
       FilterHeader(model.cars),
       h('div.car-list', Cars(dispatcher, model))
-    ])
+    ]),
+    model.selected ? Confirmation(dispatcher) : '  '
   ])
 }
